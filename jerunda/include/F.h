97734fdc8c1a45688357536bc7670d0e388d7092
NameSirname <potchtovy_jashik@mail.ru>
2018-12-27 01:25:33 +0300

    Add files via upload

---
+++ jerunda/include/F.h
@@ -0,0 +1,16 @@
+#ifndef F_h 
+#define F_h
+
+#include<complex.h>
+#include<math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+double _Complex F(double _Complex z, double _Complex c);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ jerunda/include/H.h
@@ -0,0 +1,15 @@
+#ifndef H_h 
+#define H_h
+#include"F.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+unsigned char H(double zRe, double zIm, double cRe, double cIm, int deph);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
